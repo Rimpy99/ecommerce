aa010b150a1afb02cccb1c42fa8ead1c9c886ea4
Paweł Wrona <85957282+Rimpy99@users.noreply.github.com>
2023-08-28 22:45:47 +0200

    [CLIENT] StandardNavbar component created

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -1,25 +1,7 @@
 import { Box, Typography } from "@mui/material";
 import FlexBetween from "../FlexBetween";
-import HomeIcon from '@mui/icons-material/Home';
-import LogoutIcon from '@mui/icons-material/Logout';
-import PersonIcon from '@mui/icons-material/Person';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import AdminNavbar from "./AdminNavbar";
-
-const commonNavlinkStyles = {
-    color: '#23264f',
-    fontWeight: 'bold',
-    m: 1,
-    py: 1,
-    cursor: 'pointer',
-    width: '5rem',
-    // bgcolor: '#6d75e8',
-    textAlign: 'center',
-    borderRadius: 1,
-    "&:hover": {
-        bgcolor: '#ebebf7',
-    },
-}
+import StandardNavbar from "./StandardNavbar";
 
 const Navbar = () => {
     const isAdmin = false;
@@ -43,43 +25,7 @@ const Navbar = () => {
             >
                 {
                     !isAdmin ? (
-                        <>
-                            <FlexBetween>
-                                <HomeIcon
-                                    sx={{
-                                        ...commonNavlinkStyles
-                                    }}
-                                />
-                                <Typography
-                                    sx={{
-                                        ...commonNavlinkStyles
-                                    }}
-                                >MEN</Typography>
-                                <Typography
-                                    sx={{
-                                    ...commonNavlinkStyles
-                                    }}
-                                >WOMEN</Typography>
-                                <Typography
-                                    sx={{
-                                        ...commonNavlinkStyles,
-                                        color: 'red'
-                                    }}
-                                >ON SALE</Typography>
-                            </FlexBetween>
-                            <FlexBetween>
-                                <PersonIcon
-                                    sx={{
-                                        ...commonNavlinkStyles
-                                    }}
-                                />
-                                <ShoppingCartIcon
-                                    sx={{
-                                        ...commonNavlinkStyles
-                                    }}
-                                />
-                            </FlexBetween>
-                        </>
+                        <StandardNavbar/>
                     ) : (
                         <AdminNavbar/>
                     )
---
+++ client/src/components/navbar/StandardNavbar.tsx
@@ -0,0 +1,51 @@
+import { Typography } from '@mui/material';
+import FlexBetween from "../FlexBetween";
+import HomeIcon from '@mui/icons-material/Home';
+import PersonIcon from '@mui/icons-material/Person';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { commonNavlinkStyles } from '../../styles/commonNavlinkStyles';
+
+const StandardNavbar = () => {
+
+    return(
+        <>
+            <FlexBetween>
+                <HomeIcon
+                    sx={{
+                        ...commonNavlinkStyles
+                    }}
+                />
+                <Typography
+                    sx={{
+                        ...commonNavlinkStyles
+                    }}
+                >MEN</Typography>
+                <Typography
+                    sx={{
+                    ...commonNavlinkStyles
+                    }}
+                >WOMEN</Typography>
+                <Typography
+                    sx={{
+                        ...commonNavlinkStyles,
+                        color: 'red'
+                    }}
+                >ON SALE</Typography>
+            </FlexBetween>
+            <FlexBetween>
+                <PersonIcon
+                    sx={{
+                        ...commonNavlinkStyles
+                    }}
+                />
+                <ShoppingCartIcon
+                    sx={{
+                        ...commonNavlinkStyles
+                    }}
+                />
+            </FlexBetween>
+        </>
+    )
+};
+
+export default StandardNavbar;
