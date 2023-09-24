ac7950bb581631e9315e4d69b03c4e0b846da228
Paweł Wrona <85957282+Rimpy99@users.noreply.github.com>
2023-09-24 19:08:14 +0200

    [CLIENT] Added displaying discount prices in HomePage

--- client/src/components/Product.tsx
+++ client/src/components/Product.tsx
@@ -18,7 +18,16 @@ const Product = ({product}: ProductPropsType) => {
         <Box sx={{ cursor: 'pointer', border: '1px solid lightgray', color: 'appColors.textColor' }}>
             <img style={{ width: '100%' }} src={`http://localhost:3005/images/${product.image}.jpg`} alt={`${product.name} picture`}/>
             <Typography sx={{ padding: '5px', fontSize: '18px', textTransform: 'uppercase', letterSpacing: '2px' }}>{product.name}</Typography>
-            <Typography sx={{ padding: '5px', fontSize: '18px' }}>{product.price}</Typography>
+            <Box sx={{ display: 'flex' }}>
+                <Typography 
+                    sx={{ padding: '5px', fontSize: '18px', textDecoration: product.discount_price ? 'line-through' : 'none' }}
+                >
+                    {product.price}
+                </Typography>
+                { 
+                    product.discount_price && <Typography sx={{ padding: '5px', fontSize: '18px', color: 'red' }}>{product.discount_price}</Typography>
+                }
+            </Box>
         </Box>
     )
 };
