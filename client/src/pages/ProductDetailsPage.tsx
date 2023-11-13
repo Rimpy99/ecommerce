028f21a38fb64bdf39dab4375f47011d41cc775e
Rimpy99 <70006pawel@gmail.com>
2023-11-13 20:15:30 +0100

    [CLIENT] Added fetching product info in ProductDetailsPage

--- client/src/pages/ProductDetailsPage.tsx
+++ client/src/pages/ProductDetailsPage.tsx
@@ -1,7 +1,36 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+
+type ProductType = {
+    product_id: number,
+    name: string,
+    image: string,
+    price: number,
+    sex: string,
+    color: string,
+    type: string,
+    discount_price: number | null,
+}
 
 const ProductDetailsPage = () => {
     const { productId } = useParams();
+    const [ productInfo, setProductInfo ] = useState<ProductType | null>(null);
+    const [ isError, setIsError ] = useState<boolean>(false);
+
+    const getProductInfoFromDB = async () => {
+        try{
+            const fetchProduct = await fetch(`/product/${productId}`, { method: 'GET' });
+            !fetchProduct.ok && setIsError(true);
+            const productData = await fetchProduct.json();
+            setProductInfo(productData);
+        }catch(err){
+            setIsError(true);
+        }
+    }
+
+    useEffect(() => {
+        getProductInfoFromDB();
+    }, [])
 
     return(
         <>
