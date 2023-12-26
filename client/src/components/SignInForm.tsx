1c6d9cb915af9573898bc429ca8492d6b4e2bbf4
Rimpy99 <70006pawel@gmail.com>
2023-12-26 13:25:33 +0100

    [CLIENT] Added styling and error handling to SignInForm

--- client/src/components/SignInForm.tsx
+++ client/src/components/SignInForm.tsx
@@ -1,34 +1,48 @@
 import { useState } from 'react';
-import { Box, Button, TextField } from '@mui/material';
+import { Box, Button, TextField, Typography } from '@mui/material';
 import { authFormFieldStyles } from '../styles/authFormFieldStyles';
 import { authFormButtonStyles } from '../styles/authFormButtonStyles';
+import { authFormErrorStyles } from '../styles/authFormErrorStyles';
 
 const SignInForm = () => {
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
 
+    const [emailError, setEmailError] = useState<boolean>(false);
+    const [passwordError, setPasswordError] = useState<boolean>(false);
+
+    const handleFormSubmit = () => {
+        if(email.length > 0 && password.length > 0) console.log('test')
+    }
+
     return(
         <>
             <Box sx={{ display: 'flex', flexDirection: 'column' }}>
                 <TextField
-                label="Email"
-                required
-                variant='outlined'
-                type="email"
-                value={email}
-                sx={{ ...authFormFieldStyles }}
+                    label="Email"
+                    required
+                    variant='outlined'
+                    type="email"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    sx={{ ...authFormFieldStyles }}
+                    error={emailError}
                 />
+                {emailError && <Typography sx={{ ...authFormErrorStyles }}>Email address must be correct.</Typography>}
                 <TextField
-                label="Password"
-                required
-                variant='outlined'
-                type="password"
-                value={password}
-                sx={{ ...authFormFieldStyles }}
+                    label="Password"
+                    required
+                    variant='outlined'
+                    type="password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    sx={{ ...authFormFieldStyles }}
+                    error={passwordError}
                 />
+                {passwordError && <Typography sx={{ ...authFormErrorStyles }}>Password must be at least 5 characters long.</Typography>}
                 <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
-                <Button sx={{ ...authFormButtonStyles }}>
-                    Sign up
+                <Button sx={{ ...authFormButtonStyles }} onClick={() => handleFormSubmit()}>
+                    Sign in
                 </Button>
                 </Box>
             </Box>
